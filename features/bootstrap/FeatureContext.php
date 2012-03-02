cfd68453aad23033f1453a4af18a1baa0699a711
Luis Cordova <cordoval@gmail.com>
2012-03-02 14:43:50 -0500

    first commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,75 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use PHPPeru\Parenthesis\Parenthesis;
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+    protected $inputString;
+    protected $parenthesis;
+    protected $isValid;
+
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param   array   $parameters     context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parenthesis = new Parenthesis();
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
+
+    /**
+     * @Given /^my input string is "(?P<inputText>[^"]*)"$/
+     */
+    public function myInputStringIs($inputText)
+    {
+        $this->inputString = $inputText;
+    }
+
+    /**
+     * @When /^I run parse$/
+     */
+    public function iRun()
+    {
+        $this->isValid = $this->parenthesis->validateString($this->inputString);
+    }
+
+    /**
+     * @Then /^I should get: (?P<outputValue>[^"]*)$/
+     */
+    public function iShouldGetTrue($outputValue)
+    {
+        throw new PendingException();
+    }
+
+
+}
---
+++ features/parenthesis.feature
@@ -0,0 +1,10 @@
+# features/parenthesis.feature
+Feature: parenthesis validator
+  In order to validate a string using grouping syntax
+  As a text user
+  I need to be able to validate if it follows the syntax
+
+Scenario: Validates opening and close parenthesis
+  Given my input string is "()"
+  When I run parse
+  Then I should get: true
